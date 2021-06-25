652b3c83e899064ade2e13f585c21a2772a28fd0
Nick Pierce <nickpierce7898@gmail.com>
2021-06-25 09:17:45 -0500

    first story (checking available funds) completed

---
+++ src/main/java/Lender.java
@@ -0,0 +1,14 @@
+public class Lender {
+
+    // enumerates member fields
+
+    private double _availableFunds = 0;
+
+
+    // enumerates instance methods
+
+
+    public double getAvailableFunds() {
+        return this._availableFunds;
+    }
+}
---
+++ src/test/java/LenderTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LenderTest {
+
+    Lender lender;
+
+    @BeforeEach
+    public void setLender(){
+        lender = new Lender();
+    }
+
+    @Test
+    public void testGetAvailableFunds(){
+
+        // SEAT
+
+        // Setup (no action)
+
+        // Execution (no action)
+
+        // Assertion
+        Assertions.assertEquals(0, lender.getAvailableFunds());
+
+        // Teardown
+
+    }
+
+}
