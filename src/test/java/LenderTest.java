ec1f4bdbcd6d7bbd08672a5a1a91d605f83155d0
Nick Pierce <nickpierce7898@gmail.com>
2021-06-25 11:28:31 -0500

    user story 5 commenced but not finished

--- src/test/java/LenderTest.java
+++ src/test/java/LenderTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LenderTest {
@@ -193,4 +195,129 @@ public class LenderTest {
         // Teardown (no action)
 
     }
+
+    /*
+    @Test
+    public void testGetApprovedLoan(){
+
+        // SEAT
+
+        // Test 1 (invalid -- application approved BUT lender does not have sufficient available funds)
+        final double testOneLoanAmount  = 200000;
+        final double testOneSavingsAmount  = 50000;
+        final int testOneDti  = 35;
+        final int testOneCreditScore = 621;
+        final boolean testOneExpected = false;
+
+        // no partial loan needed -- loan not granted/created
+
+        // lenders initial funds availability
+        final double lenderAvailability = 100000;
+
+        // Execution
+        lender.depositFunds(lenderAvailability);
+
+        final Optional<Loan> testOneActualLoan = lender.getPendingLoanForApplicant(
+                testOneLoanAmount,
+                testOneSavingsAmount,
+                testOneDti,
+                testOneCreditScore
+        );
+
+        // Assertion
+        assertEquals(testOneExpected, testOneActualLoan.isPresent(), "testGetApprovedLoan: test one");
+
+
+        // Termination (no action)
+
+        // Test 2 (invalid -- regardless of lender's fund availability)
+        // applicant's loan approval request (invalid -- regardless of lender's fund availability)
+
+        // no partial loan needed -- loan not granted/created
+
+        final double testTwoLoanAmount  = 100000;
+        final double testTwoSavingsAmount  = 24000;
+        final int testTwoDti  = 36;
+        final int testTwoCreditScore = 620;
+        final boolean testTwoExpected = false;
+
+        // Execution
+
+        final Optional<Loan> testTwoActualLoan = lender.getPendingLoanForApplicant(
+                testTwoLoanAmount,
+                testTwoSavingsAmount,
+                testTwoDti,
+                testTwoCreditScore
+        );
+
+        // Assertion
+        assertEquals(testTwoExpected, testTwoActualLoan.isPresent(), "testGetApprovedLoan: test two");
+
+
+        // Termination (no action)
+
+
+        // Test 3 (valid && lender has funds)
+        final double testThreeLoanAmount  = 100000;
+        final double testThreeSavingsAmount  = 25000;
+        final int testThreeDti  = 35;
+        final int testThreeCreditScore = 621;
+        final boolean testThreeExpected = true;
+
+        // Setup
+        // applicant's loan approval request (valid && lender has funds)
+
+        final Loan partialExpectedLoan = new Loan(){{
+            setLoanAmount(100000);
+            setLoanStatus(Loan.LoanStatus.PENDING);
+        }};
+
+        final double validInputLoanAmount  = 100000;
+        final double validInputSavingsAmount  = 25000;
+        final int validInputDti  = 35;
+        final int validInputCreditScore = 621;
+        final boolean validExpected = true;
+
+
+        // Execution
+        final Optional<Loan> testFourActualLoanOptional = lender.getPendingLoanForApplicant(
+                inputLoanAmount,
+                inputSavingsAmount,
+                inputDti,
+                inputCreditScore
+        );
+
+        // Assertion
+
+        assertEquals(validExpected, testFourActualLoanOptional.isPresent(), "testGetApprovedLoan: test three");
+
+        // extract Loan
+        final Loan actualLoan = testFourActualLoanOptional.orElseThrow();
+
+        // Loan: loan amount
+        assertEquals(
+                partialExpectedLoan.getLoanAmount(),
+                actualLoan.getLoanAmount(),
+                "Loan amounts are not equal. Please revise."
+        );
+
+        // Loan: loan status
+        assertEquals(
+                partialExpectedLoan.getLoanStatus(),
+                actualLoan.getLoanStatus(),
+                "Loan statuses are not equal. Please revise."
+        );
+
+        // Lender: available funds
+        assertEquals(
+                ,
+                actualLoan.getLoanAmount(),
+                "Loan amounts are not equal. Please revise."
+        );
+
+
+    }
+
+    
+     */
 }
